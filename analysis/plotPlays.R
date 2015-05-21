b174d0b42e6fd433ee6ddb8aae12de6e73d9a325
Adam Wespiser <adamwespiser@gmail.com>
2015-05-20 20:59:27 -0400

    add plotting code

---
+++ analysis/plotPlays.R
@@ -0,0 +1,10 @@
+library(ggplot2)
+
+library("./analysis/readInPlays.R")
+
+# yards per play
+# pr score
+# punt distribution
+# expected point per field position
+# yards per field position(all plays)
+
