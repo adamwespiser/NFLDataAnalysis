6fdff47cb02ba81674b45930082e761ae0d2effd
Adam Wespiser <adamwespiser@gmail.com>
2015-05-22 13:01:10 -0400

    update ignore

--- analysis/libInstall.R
+++ analysis/libInstall.R
@@ -3,3 +3,9 @@ install.packages('dplyr')
 install.packages("reshape2")
 install.packages('data.table')
 install.packages('ggplot2')
+
+
+
+#.libPaths( c( .libPaths(), "~/userLibrary") )
+#export RSTUDIO_WHICH_R=/usr/local/bin/R
+#install.packages("ggplot2", lib="/data/Rpackages/")
